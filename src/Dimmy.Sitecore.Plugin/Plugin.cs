fdb73a2f89e8f37e170d2e62e05f3cabdc4a3212
Gravypower <aaron@gravypower.net>
2020-08-10 19:08:48 +1000

    Added decorator to start command

--- src/Dimmy.Sitecore.Plugin/Plugin.cs
+++ src/Dimmy.Sitecore.Plugin/Plugin.cs
@@ -1,4 +1,7 @@
 ﻿using Dimmy.Cli;
+using Dimmy.Cli.Commands.Project;
+using Dimmy.Cli.Commands.Project.SubCommands;
+using Dimmy.Sitecore.Plugin.Versions._10._0._0;
 using SimpleInjector;
 
 namespace Dimmy.Sitecore.Plugin
@@ -6,7 +9,14 @@ namespace Dimmy.Sitecore.Plugin
     public class Plugin : IPlugin
     {
         public void Bootstrap(Container container)
+        { 
+            container.RegisterDecorator(
+            typeof(IProjectSubCommand), typeof(StartDecorator), Predicate);
+        }
+
+        private bool Predicate(DecoratorPredicateContext context)
         {
+            return context.ImplementationType == typeof(Start);
         }
     }
 }
---
+++ src/Dimmy.Sitecore.Plugin/Versions/10.0.0/StartDecorator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CommandLine;
+using Dimmy.Cli.Commands.Project;
+using Dimmy.Cli.Commands.Project.SubCommands;
+
+namespace Dimmy.Sitecore.Plugin.Versions._10._0._0
+{
+    public class StartDecorator : ProjectSubCommand<StartArgument>
+    {
+        private readonly IProjectSubCommand _decorated;
+
+        public StartDecorator(IProjectSubCommand decorated)
+        {
+            _decorated = decorated;
+        }
+        
+        public override Command GetCommand()
+        {
+            return _decorated.GetCommand();
+        }
+
+        protected override void CommandAction(StartArgument arg)
+        {
+            Console.WriteLine("HELLO FROM StartDecorator");
+            
+            _decorated.CommandAction(arg);
+        }
+    }
+}
